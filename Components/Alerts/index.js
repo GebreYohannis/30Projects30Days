fd20334e78efb1413f032664f92320b40512ac8e
Demis Getachew <demisgech@gmail.com>
2024-10-17 23:11:32 +0300

    Build alert component.

---
+++ Components/Alerts/index.js
@@ -0,0 +1,21 @@
+const alertPlaceholder = document.querySelector(".liveAlertPlaceholder");
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = [
+    `
+   <div class="alert alert--${type} alert--dismissible" role="alert">`,
+    `<div class="message">${message}</div>`,
+    `<button type="button" class="btn-close">X</button></div>`,
+  ].join("");
+  alertPlaceholder.append(wrapper);
+  wrapper.querySelector(".btn-close").addEventListener("click", () => {
+    wrapper.remove();
+  });
+};
+
+const alertTrigger = document.querySelector("#liveAlertBtn");
+if (alertTrigger) {
+  alertTrigger.addEventListener("click", () => {
+    appendAlert("Nice!, you triggerd this alert!", "success");
+  });
+}
